29266c55edf15694082295f44c2434d709f82692
Guido Rombola <guidorombola@gmail.com>
2018-11-05 11:05:48 -0300

    Se agrego cantidad maxima de intentos de conexion

--- src/ConectorWiFi.cpp
+++ src/ConectorWiFi.cpp
@@ -1,20 +1,27 @@
 #include "ConectorWiFi.hpp"
 #include <WiFi.h>
 
+#define MAX_INTENTOS_CONEXION 10
+
 ConectorWiFi::ConectorWiFi(const char* ssid, const char* clave){
     this->ssid = ssid;
     this->clave = clave;
 }
 
 void ConectorWiFi::realizarConexion(){
+    int intentosConexion = 0;
     WiFi.begin(this->ssid, this->clave);
 
-    while (!this->estaConectado()) {
+    while (!this->estaConectado() && intentosConexion < MAX_INTENTOS_CONEXION) {
         delay(1000);
         Serial.println("Conectando al WiFi..");
+        intentosConexion++;
     }
-
-    Serial.println("Conexión exitosa a la red!"); 
+    if(this->estaConectado()){
+        Serial.println("Conexión exitosa a la red!");
+    } else {
+        Serial.println("No se pudo conectar a red WiFi");
+    }
 }
 
 bool ConectorWiFi::estaConectado(){
