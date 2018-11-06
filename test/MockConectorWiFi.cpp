3bbcf7deeeeb850f0491d6034c3965f12d3f6400
Guido Rombola <guidorombola@gmail.com>
2018-11-06 14:06:56 -0300

    Correccion en definicion del mock

--- test/MockConectorWiFi.cpp
+++ test/MockConectorWiFi.cpp
@@ -1,7 +1,7 @@
 #include <GestorDeConexion.hpp>
 #include "gmock/gmock.h"
 
-class MockConectorCI : public GestorDeConexion {
+class MockConectorWiFi : public GestorDeConexion {
     public:
     MOCK_METHOD0(realizarConexion, void());
     MOCK_METHOD0(estaConectado, bool());
